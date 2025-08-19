e14d2e11aeb145637cee1e45102463bc5c35464a
Fernandogomes12398 <fernandogomes7165@gmail.com>
2025-08-19 16:11:59 -0300

    Add files via upload

--- pacman.c
+++ pacman.c
@@ -1,75 +1,68 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-//mapa
-
-char mapa[10] [11] {
-    "###########"
-    "#.........#"
-    "#.#######.#"
-    "#.........#"
-    "#####.#####"
-    "#.........#"
-    "#.#######.#"
-    "#.........#"
-    "#.........#"
+// mapa
+char mapa[10][11] = {
+    "###########",
+    "#.........#",
+    "#.#######.#",
+    "#.........#",
+    "#####.#####",
+    "#.........#",
+    "#.#######.#",
+    "#.........#",
+    "#.........#",
     "###########"
 };
 
-//imprimir
-
-void imprimeMapa(int px, py) {
-    system ("clear");
-
-    for (i = 0; i<10; i++){
-        for (j = 0; j<10; j++){
-            if (i == px && j == py ) printf("P"); //pacman
-            else printf ("%c", mapa [i] [j]);
+// imprimir
+void imprimeMapa(int px, int py) {
+    system("clear");
+    int i, j;
+    for (i = 0; i < 10; i++) {
+        for (j = 0; j < 11; j++) {
+            if (i == px && j == py)
+                printf("P"); // pacman
+            else
+                printf("%c", mapa[i][j]);
         }
-        printf ("\n");
+        printf("\n");
     }
-
-
 }
 
-//mover pacman
-
-void mover(char comando, int *px , int *py){
+// mover pacman
+void mover(char comando, int *px, int *py) {
     int novoX = *px, novoY = *py;
 
+    if (comando == 'w') novoX--; // cima
+    if (comando == 's') novoX++; // baixo
+    if (comando == 'a') novoY--; // esquerda
+    if (comando == 'd') novoY++; // direita
 
-    if(comando == 'w') novoX--; //cima
-    if(comando == 's') novoX++; //baixo
-    if(comando == 'a') novoY--; //esquerda
-    if(comando == 'd') novoX++; //direita
-
-    if(mapa[novoX][novoY]  != '#') {
+    // checa limites
+    if (novoX >= 0 && novoX < 10 && novoY >= 0 && novoY < 11 && mapa[novoX][novoY] != '#') {
         *px = novoX;
         *py = novoY;
 
-        if(mapa[*px][*py] == '.') {
+        if (mapa[*px][*py] == '.') {
             mapa[*px][*py] = ' ';
         }
     }
 }
-//progama principal
 
-
-int main(){
-    int px, py;
+// programa principal
+int main() {
+    int px = 1, py = 1; // posição inicial válida
     char comando;
 
-    while(1) {
+    while (1) {
         imprimeMapa(px, py);
 
-        printf("\nUse W A S D para mover (CRTL+C para sair): ");
-        scanf (" %c", &comando);
+        printf("\nUse W A S D para mover (CTRL+C para sair): ");
+        scanf(" %c", &comando);
 
-        mover (comando, &px, &py);
-
+        mover(comando, &px, &py);
     }
 
     return 0;
 }
-
-
